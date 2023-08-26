7d3b8c727e31cec9e2e902f9b56281192677c8cd
Pooja Priya <poojapriya1399@gmail.com>
2023-08-27 00:11:47 +0530

    change background color and image

---
+++ app.js
@@ -0,0 +1,8 @@
+function changeImage(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeBackground(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
